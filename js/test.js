e24af695d6a8758cb508e312ab0e732a15cc38e0
Maverick <supacode@gmail.com>
2020-01-29 04:50:19 +0100

    Fix navbar on scroll
    Delete post.html page

---
+++ js/test.js
@@ -0,0 +1,13 @@
+const mainNav = document.querySelector("#main__header");
+const navScrollClass = "main__header--scrolled";
+
+const fixNav = () => {
+  const navHeight = mainNav.offsetHeight;
+  if (window.scrollY > navHeight / 2) {
+    mainNav.classList.add(navScrollClass);
+  } else {
+    mainNav.classList.remove(navScrollClass);
+  }
+};
+
+window.addEventListener("scroll", fixNav);
