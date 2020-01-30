05e9884d13028ea07ec17d654f3e03cb3a4211f5
Maverick <supacode@gmail.com>
2020-01-31 00:49:35 +0100

    Create featured blog section

--- js/test.js
+++ js/test.js
@@ -2,8 +2,7 @@ const mainNav = document.querySelector('#main__header');
 const navScrollClass = 'main__header--scrolled';
 
 const fixNav = () => {
-  const navHeight = mainNav.offsetHeight;
-  if (window.scrollY > navHeight / 2) {
+  if (window.scrollY > mainNav.offsetHeight / 2) {
     mainNav.classList.add(navScrollClass);
   } else {
     mainNav.classList.remove(navScrollClass);
